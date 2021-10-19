81657c795bdf1b8184c219f08bebd062aba7ad87
Prayogabrd Ⓥ <72582850+P677hl@users.noreply.github.com>
2021-10-19 16:53:35 +0700

    Add files via upload

--- assets/js/downloader.js
+++ assets/js/downloader.js
@@ -1,6 +1,6 @@
 // code by PrayogaBRD //
 var utama = 'https://bit.ly/';
 var widget = utama + '/2Yn2AZ0';
-var widgetHtml = '<iframe id="ifr" src="' + widget + '" style="border:0;width:100%;height:650px;padding: 0;"></iframe>';
+var widgetHtml = '<iframe id="ifr" src="' + widget + '" style="border:0;width:100%;height:660px;padding: 0;"></iframe>';
 var isNew = document.getElementById("content");
 document.getElementById("content").innerHTML = widgetHtml;
